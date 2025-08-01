8ba1a6b230d89aaa87f281f817f344ad505f8670
Pedro Henrique Silva Souza <pedro.souza@acovisa.local>
2025-08-01 14:47:32 -0300

    Projeto alura

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Bem vindo ao react
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
       </header>
     </div>
   );
